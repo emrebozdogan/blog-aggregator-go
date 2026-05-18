b343ebbff8552f493e43929b2901d3c7a1043191
emrebozdogan <emrbzdgn@icloud.com>
2026-05-18 15:15:34 +0300

    Add feeds schema migration

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE feeds (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  name TEXT NOT NULL,
+  url TEXT NOT NULL UNIQUE,
+  user_id UUID REFERENCES users (id) ON DELETE CASCADE NOT NULL,
+  CONSTRAINT fk_feeds_users
+    FOREIGN KEY (user_id)
+    REFERENCES users (id)
+);
+
+-- +goose Down
+DROP TABLE feeds;
